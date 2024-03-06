0fcc56c0f9dd32c93737d35d94cc1b217a4fb1c9
feihuan1 <ericrealtor1997@gmail.com>
2024-03-06 15:09:07 -0800

    Initial Commit

---
+++ ImageGallery/ImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryApp.swift
+//  ImageGallery
+//
+//  Created by Feihuan Peng on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
